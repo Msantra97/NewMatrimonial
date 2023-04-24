77811776a6c73bd85b1dd0494177b5c7a7108710
Msantra97 <monojsantra8013@gmail.com>
2023-04-25 00:36:36 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import About from '@/components/About'
 import Footer from '@/components/Footer'
 import Gallery from '@/components/Gallery'
 import Nav from '@/components/Nav'
+import Profile from '@/components/Profile'
 import Service from '@/components/Service'
 import Story from '@/components/Story'
 import React from 'react'
@@ -9,12 +10,13 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      <Nav/>
+      {/* <Nav/>
       <Gallery/>
       <About/>
       <Story/>
       <Service/>
-      <Footer/>
+      <Footer/> */}
+      <Profile/>
     </div>
   )
 }
