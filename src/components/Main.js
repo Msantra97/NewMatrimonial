1ca3f0f037cad736ca2287c08372a8339d052764
Msantra97 <monojsantra8013@gmail.com>
2023-04-30 01:05:08 +0530

    change

--- src/components/Main.js
+++ src/components/Main.js
@@ -67,7 +67,7 @@ export default function Main() {
         <span class="text-sm text-gray-500 dark:text-gray-400">College Student</span>
         <span class="text-sm text-gray-500 dark:text-gray-400">24,5"4"</span>
         <span class="text-sm text-gray-500 dark:text-gray-400">Hindu,Bengali</span>
-        <span class="text-sm text-gray-500 dark:text-gray-400">126, Ram Bagan, Kolkata, West Bengal 700006</span>
+        <span class="text-sm text-gray-500 dark:text-gray-400">126, Ram Bagan, Kolkata,</span>
        
         <div class="flex mt-4 space-x-3 md:mt-6">
             <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Follow</a>
