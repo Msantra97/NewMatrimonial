6259f44908e07ca63c20be475323f819c45f3722
Mukesh Roy <mkshr8@gmail.com>
2023-04-30 14:11:46 +0530

    toggle

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,14 @@
 import Main from "@/components/Main";
+import NewNav from "@/components/NewNav";
 import ProfileCard from "@/components/ProfileCard";
 import React from "react";
 
 export default function index() {
   return (
     <div>
+        <NewNav/>
       <Main >
-        <ProfileCard />
+      
          </Main>
 
     </div>
